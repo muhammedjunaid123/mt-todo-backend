125094509bd68e35c3b390d7658ed2419024270e
junaid <junaidvinu133@gmail.com>
2024-08-04 20:39:07 +0530

    feat :v1

--- src/public/files/day one.md
+++ src/public/files/day one.md
@@ -33,3 +33,15 @@
 - [x] drink water
 - [x] coding
 
+# day one
+
+ **Summary**: 2/3 Todos Completed 
+
+# Pending 
+- [ ] running
+
+
+# Completed 
+- [x] coding
+- [x] drink water
+
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -9,11 +9,11 @@ import {
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const route = Router();
-route.route('/exportToGist').get(exportGist)
 route.use(verifyJWT);
 route.route("/create").post(createNewProject);
 route.route("/titleUpdate").patch(updateProject);
 route.route("/getUserProject").get(userProject);
 route.route('/getProject').get(getProject)
+route.route('/exportToGist').get(exportGist)
 
 export default route;
