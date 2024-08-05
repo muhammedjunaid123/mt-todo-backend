6b01cb58712bdffecda48ab2e6cea1f2d2ea2357
junaid <junaidvinu133@gmail.com>
2024-08-05 20:03:35 +0530

    feat :added login api test

--- src/test/all.test.js
+++ src/test/all.test.js
@@ -40,4 +40,48 @@ describe("POST /api/v1/user/register", () => {
   });
 });
 
+//user login
+describe("POST /api/v1/user/login", () => {
+  afterAll(async () => {
+    await userModel.deleteOne({ email: userData["email"] });
+  });
+  test("should login a user successfully", async () => {
+    await supertest(app).post("/api/v1/user/register").send(userData);
+    const response = await supertest(app)
+      .post("/api/v1/user/login")
+      .send(userData);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.statusCode).toBe(200);
+    expect(response.body.message).toBe("Success");
+    expect(response.body.success).toBe(true);
+    token = response.body.token;
+  });
 
+  test("should return error if user is not found", async () => {
+    const userDataFake = {
+      email: "nonexistent@example.com",
+      password: "SecurePass123!",
+    };
+
+    const response = await supertest(app)
+      .post("/api/v1/user/login")
+      .send(userDataFake);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.statusCode).toBe(404);
+    expect(response.body.message).toBe("user not found");
+  });
+
+  test("should return error if password is incorrect", async () => {
+    const userdataWrongPass = { ...userData };
+    userdataWrongPass["password"] = "WrongPassword123!";
+
+    const response = await supertest(app)
+      .post("/api/v1/user/login")
+      .send(userdataWrongPass);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.statusCode).toBe(401);
+    expect(response.body.message).toBe("incorrect password");
+  });
+});
